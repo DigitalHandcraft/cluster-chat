c280a682bfe6ce1fdc65e07c84b5bb1ac4a0f43e
rk0der <ryo.alter@gmail.com>
2016-10-10 01:29:30 +0900

    fix bugs

--- lib/ws_front.rb
+++ lib/ws_front.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 require 'em-websocket'
 require 'chat_core'
 require 'json'
+require 'time'
 
 class WSFront
   def initialize chat_core, args
@@ -18,18 +19,24 @@ class WSFront
     @events << event
   end
 
-  def broadcast message
-    @clients.each{|c| c[:conn].send(message)}
+  def broadcast ctx_id, text
+    dests = @clients.select{|c| c[:user][:ctx_id] == ctx_id}
+    dests.each{|c| c[:conn].send(text)}
   end
 
-  def multicast names, message
-    dests = @clients.select {|c| names.include? c[:name]}
-    dests.each {|c| c[:conn].send(message)}
+  def multicast ctx_id, names, text
+    dests0 = @clients.select{|c| c[:user][:ctx_id] == ctx_id}
+    dests1 = dests0.select{|c| names.include? c[:user][:name]}
+    dests1.each {|c| c[:conn].send(text)}
+  end
+
+  def loopback client, text
+    client[:conn].send(text)
   end
 
   def start
     EM::WebSocket.start({host: @host, port: @port}) do |conn|
-      client = {name: '', conn: conn}
+      client = {conn: conn}
 
       conn.onopen do
         @clients << client
@@ -46,19 +53,21 @@ class WSFront
           case d[:dest]
           when 'users/join'
             client[:user] = @core.join d[:ctx_id], d[:name]
+            p @clients
             # broadcast
           when 'users/info'
           when 'users/list'
           when 'users/leave'
           when 'statuses/load'
-            # d[:start] should be like '2016/10/05 00:50:30'
-            t0 = d[:start]
-            t1 = d[:end]
-            statuses = @core.load_statuses client[:ctx_id], t0, t1
+            # d[:start] should be like '2016-10-05 00:50:30 +0900'
+            t0 = Time.parse(d[:start]).utc
+            t1 = Time.parse(d[:end]).utc
+            statuses = @core.load_statuses client[:user][:ctx_id], t0, t1
             # bug in the case of duplicated name
-            multicast [client[:name]]
+            loopback client, JSON.unparse(statuses)
           when 'statuses/add'
-            @core.send client[:user][:ctx_id], d[:text]
+            res = @core.send client[:user][:ctx_id], d[:text]
+            broadcast client[:user][:ctx_id], JSON.unparse(res[:data])
           when 'contexts/info'
           else
             puts 'wrong type'
